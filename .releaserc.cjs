cc1b4afbf63b9f43ab711092089c0698fbcca502
Dafydd Jones <dafydd@techneg.it>
2024-11-08 02:08:55 +0000

    ci: update release note section headings

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,28 +1,36 @@
 const commitTypes = [
-  { breaking: true,     release: 'major' },
-  {"type": "feat", "section": "Features"},
-  {"type": "fix", "section": "Bug Fixes"},
-  { type: 'new', section: 'New Packages'},
-  { type: 'update', section: 'Updates'},
-  { type: 'del', section: 'Removals'}
+	{ breaking: true, release: "major" },
+	{ type: "feat", section: "Features" },
+	{ type: "fix", section: "Bug Fixes" },
+	{ type: "new", section: "New Packages" },
+	{ type: "update", section: "Updates" },
+	{ type: "del", section: "Removals" },
+	{ type: "docs", section: "Documentation" },
+	{ type: "test", section: "Testing" },
+	{ type: "perf", section: "Performance Improvements" },
+	{ type: "revert", section: "Reversions" },
+	{ type: "refactor", section: "Code Refactoring" },
+	{ type: "style", section: "Style Changes" },
+	{ type: "ci", section: "Continuous Integration" },
+	{ type: "chore", section: "Maintenance" },
 ];
 
 const releaseRules = [
-  {type: 'update', release: 'patch'},
-  {type: 'new', release: 'minor'},
-  {type: 'del', release: 'major'},
+	{ type: "update", release: "patch" },
+	{ type: "new", release: "minor" },
+	{ type: "del", release: "major" },
 ];
 
 const config = {
-  plugins: [ ["@semantic-release/commit-analyzer", {releaseRules: releaseRules}],
-              "@semantic-release/release-notes-generator",
-              "@semantic-release/github"
-  ],
-  preset: 'conventionalcommits',
-  presetConfig:
-  {
-    types: commitTypes,
-  },
+	plugins: [
+		["@semantic-release/commit-analyzer", { releaseRules: releaseRules }],
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/github",
+	],
+	preset: "conventionalcommits",
+	presetConfig: {
+		types: commitTypes,
+	},
 };
 
 module.exports = config;
